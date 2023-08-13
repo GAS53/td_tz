071f09ac160e8b4ef27a6d8dcacd5d40878250e0
Gleb Sviridoff <gas53@bk.ru>
2023-08-13 18:21:28 +0300

    init models

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class BaseUser(AbstractUser):
+    id = models.BigAutoField(verbose_name='user id')
+    birthday = models.PositiveIntegerField(verbose_name="birthday")
+    email = models.CharField(verbose_name="email", max_length=40, unique=True)
+    first_name = models.CharField(verbose_name="name", max_length=60)
+    last_name = models.CharField(verbose_name="surname", max_length=60)
+    patronymic = models.CharField(verbose_name="patronymic", max_length=40, default="")
+    password = models.CharField(verbose_name="password", max_length=40)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    is_driver = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name} ({self.last_name})"
--- driverapp/models.py
+++ driverapp/models.py
@@ -1,3 +1,21 @@
+from authapp.models import BaseUser
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    id = models.BigIntegerField(verbose_name='company_id', primary_key=True, unique=True)
+    full_name = models.CharField(verbose_name="full company name", max_length=200)
+    short_name = models.CharField(verbose_name="full company name", max_length=50)
+
+
+class DriverStatus(models.Model):
+    value = models.CharField(verbose_name='value', max_length=10, default='not activ')
+    description = models.CharField(verbose_name='description', max_length=200)
+
+
+class DriverLog(BaseUser):
+    company_id = models.ForeignKey(Company, on_delete=models.SET_NULL)
+    status = models.ForeignKey(DriverStatus, on_delete=models.SET_DEFAULT, default='not activ')
+
+    def __str__(self):
+        return f"{self.first_name} ({self.last_name})"
